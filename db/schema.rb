a6e0ec992123476cbc52ffc04d81e174722a9307
Shubhampatel25 <73432071+Shubhampatel25@users.noreply.github.com>
2021-06-05 14:11:54 +0530

    style css

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,16 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_141627) do
-
+ActiveRecord::Schema.define(version: 2021_06_04_053247) do
   create_table "properties", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -12,4 +22,5 @@ ActiveRecord::Schema.define(version: 2021_06_02_141627) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  
 end
